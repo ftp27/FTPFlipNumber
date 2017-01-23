c5fbe86f2c3ba49fa8741477678903aad4c133fa
A. Cherepanov <ftp27host@gmail.com>
2017-01-23 08:55:25 +0300

    Added flip animation

--- FTPFlipNumber/Classes/FTPFlipNumber.swift
+++ FTPFlipNumber/Classes/FTPFlipNumber.swift
@@ -12,6 +12,9 @@ import UIKit
 class FTPFlipNumber: UIView {
     
     
+    let animDuration: TimeInterval = 1
+    
+    
     //      _____
     // ____|_    |--- secondTopLayer
     // \     \---|--- firstTopLayer
@@ -25,6 +28,8 @@ class FTPFlipNumber: UIView {
     let secondTopLayer    = CALayer()
     let firstBottomLayer  = CALayer()
     let secondBottomLayer = CALayer()
+    
+    var bufferContents:CGImage?
   
     @IBInspectable
     var displaedNumber:UInt = 0 {
@@ -56,18 +61,35 @@ class FTPFlipNumber: UIView {
     }
     
     func viewInit() {
-        layer.addSublayer(firstBottomLayer)
         layer.addSublayer(secondBottomLayer)
-        layer.addSublayer(firstTopLayer)
         layer.addSublayer(secondTopLayer)
+        layer.addSublayer(firstTopLayer)
+        layer.addSublayer(firstBottomLayer)
+        
+        let topAnctor    = CGPoint(x: 0.5, y: 1)
+        let bottomAnctor = CGPoint(x: 0.5, y: 0)
+        
+        firstTopLayer.anchorPoint     = topAnctor
+        secondTopLayer.anchorPoint    = topAnctor
+        firstBottomLayer.anchorPoint  = bottomAnctor
+        secondBottomLayer.anchorPoint = bottomAnctor
+        
+        firstTopLayer.backgroundColor     = UIColor.white.cgColor
+        secondTopLayer.backgroundColor    = UIColor.white.cgColor
+        firstBottomLayer.backgroundColor  = UIColor.white.cgColor
+        secondBottomLayer.backgroundColor = UIColor.white.cgColor
+        
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.onTouch)))
     }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
         let height = layer.bounds.height/2
         let width  = layer.bounds.width
-        let topLayerRect    = CGRect(x: 0, y: 0,      width: width, height: height)
-        let bottomLayerRect = CGRect(x: 0, y: height, width: width, height: height)
+        let topLayerRect    = CGRect(x: 0, y: 0,
+                                     width: width, height: height)
+        let bottomLayerRect = CGRect(x: 0, y: height,
+                                     width: width, height: height)
         
         firstTopLayer.frame     = topLayerRect
         secondTopLayer.frame    = topLayerRect
@@ -79,6 +101,36 @@ class FTPFlipNumber: UIView {
                                 self.firstTopLayer.contents = topImage?.cgImage
                                 self.firstBottomLayer.contents = bottomImage?.cgImage
         }
+        
+      /*  FTPImageCreator.images(String(displaedNumber+1),
+                               targetSize: layer.bounds.size) { (topImage:UIImage?, bottomImage:UIImage?) in
+                                self.secondTopLayer.contents = topImage?.cgImage
+                                self.secondBottomLayer.contents = bottomImage?.cgImage
+        }*/
+    }
+    
+    func onTouch() {
+        
+        displaedNumber += 1
+        FTPImageCreator.images(String(displaedNumber+1),
+        targetSize: layer.bounds.size) { (topImage:UIImage?, bottomImage:UIImage?) in
+            self.secondTopLayer.contents = topImage?.cgImage
+            self.secondBottomLayer.contents = self.firstBottomLayer.contents
+            self.bufferContents = bottomImage?.cgImage
+        }
+        
+        let topAnim = CABasicAnimation(keyPath: "transform")
+        topAnim.duration = animDuration/2
+        topAnim.repeatCount = 1
+        topAnim.fromValue = NSValue.init(caTransform3D:
+            CATransform3DMakeRotation(0, 1, 0, 0))
+        topAnim.toValue = NSValue.init(caTransform3D:
+            CATransform3DMakeRotation((CGFloat)(-M_PI_2), 1, 0, 0))
+        topAnim.isRemovedOnCompletion = true
+        topAnim.delegate = self
+        topAnim.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut)
+        firstTopLayer.add(topAnim, forKey: "top")
+        
     }
 
     /*
@@ -89,4 +141,28 @@ class FTPFlipNumber: UIView {
     }
     */
 
+}
+
+extension FTPFlipNumber: CAAnimationDelegate {
+    func animationDidStart(_ anim: CAAnimation) {
+        
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if flag {
+            firstTopLayer.contents = secondTopLayer.contents
+            let bottomAnim = CABasicAnimation(keyPath: "transform")
+            bottomAnim.duration = animDuration/2
+            bottomAnim.repeatCount = 1
+            bottomAnim.fromValue = NSValue.init(caTransform3D:
+                CATransform3DMakeRotation((CGFloat)(M_PI_2), 1, 0, 0))
+            bottomAnim.toValue = NSValue.init(caTransform3D:
+                CATransform3DMakeRotation(0, 1, 0, 0))
+            bottomAnim.isRemovedOnCompletion = true
+           // bottomAnim.delegate = self
+            bottomAnim.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseIn)
+            firstBottomLayer.add(bottomAnim, forKey: "bottom")
+            firstBottomLayer.contents = self.bufferContents
+        }
+    }
 }
